a0452dfc8747f374f821b95df7cfc710aa5f0281
HOWARD1021 <44057435+HOWARD1021@users.noreply.github.com>
2022-12-13 15:00:09 +0800

    Vue.createApp

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-Vue.create({
+Vue.createApp({
     data() {
         return {
             name:''
@@ -11,4 +11,35 @@ Vue.create({
 
     }
 }).mount('#assignment')
+
+
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       counter: 0,
+//       name:"",
+//       enterName:"",
+//     };
+//   },
+//   methods: {
+//     add() {
+//       this.counter++;
+//     },
+//     reduce() {
+//       this.counter =this.counter -1 ;
+//     },
+//     setName(event,second){
+//       this.name = event.target.value + ' ' +second
+//     },
+//     setEnterName(event) {
+//       this.enterName  = event.target.value
+//     },
+//     submitForm(event){
+//       // event.preventDefault();
+//       alert('submit')
+//     }
+//   },
+// });
+
+// app.mount("#events");
 
